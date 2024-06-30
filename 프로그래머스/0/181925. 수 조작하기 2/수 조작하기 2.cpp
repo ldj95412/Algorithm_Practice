a20112190f0b753aa9f28e1870e30a26d1600b1f
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-01 02:08:52 +0900

    [level 0] Title: 수 조작하기 2, Time: 0.95 ms, Memory: 5.88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) 
+{
+    string answer = "";
+    for(int i = 1; i < numLog.size(); i++)
+    {
+        if(numLog[i] - numLog[i-1] == 1)
+        {
+            answer.push_back('w');
+        }
+        else if(numLog[i] - numLog[i-1] == 10)
+        {
+            answer.push_back('d');
+        }
+        else if(numLog[i] - numLog[i-1] == -1)
+        {
+            answer.push_back('s');
+        }
+        else if(numLog[i] - numLog[i-1] == -10)
+        {
+            answer.push_back('a');
+        }
+    }
+    return answer;
+}
