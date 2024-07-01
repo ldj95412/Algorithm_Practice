1b2416ee4b28e3848cb9b43b35eee415ea66d6dd
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-01 16:08:47 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) 
+{
+    int answer = 0;
+    for(int i = 0; i < date1.size(); i++)
+    {
+        if(date1[i] < date2[i])
+        {
+            answer = 1;
+            break;
+        }
+        else if(date1[i] > date2[i])
+        {
+            answer = 0;
+            break;
+        }
+    }
+    return answer;
+}
