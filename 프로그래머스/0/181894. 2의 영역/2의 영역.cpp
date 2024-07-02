4e9efba1141218ec8c2d1b0f45990f1b204a71cd
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-03 01:44:32 +0900

    [level 0] Title: 2의 영역, Time: 18.59 ms, Memory: 19.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    vector<int> idx;
+    for(int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i] == 2) idx.push_back(i);
+    }
+    
+    if(idx.empty()) answer.push_back(-1);
+    else
+    {
+        for(int i = idx[0]; i <= idx[idx.size()-1]; i++ )
+        {
+            answer.push_back(arr[i]);
+        }
+    }
+    return answer;
+}
