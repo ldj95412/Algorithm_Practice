0dc07ae0f68e202b4849a7f1f10c29841ea3cb18
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-06 15:34:46 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) 
+{
+    int answer = 0;
+    if(ineq == ">" && eq == "=") answer = (n >= m);
+    else if(ineq == "<" && eq == "=") answer = (n <= m);
+    else if(ineq == ">" && eq == "!") answer = (n > m);
+    else if(ineq == "<" && eq == "!") answer = (n < m);
+    
+    return answer;
+}
