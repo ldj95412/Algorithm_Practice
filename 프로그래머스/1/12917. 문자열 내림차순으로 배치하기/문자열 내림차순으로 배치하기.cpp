e228f690b7daa425a7b30ad76f1dfd48196e764f
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-13 15:32:45 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(char a, char b)
+{
+    return a > b;
+}
+
+string solution(string s) 
+{
+    string answer = "";
+    answer = s;
+    sort(answer.begin(), answer.end(), compare);
+    return answer;
+}
