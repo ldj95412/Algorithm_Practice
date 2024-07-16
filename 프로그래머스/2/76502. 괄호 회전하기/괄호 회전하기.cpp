45689761e5af36ccb53bac24144c54cb9b5d5974
이동준 <144770957+ldj95412@users.noreply.github.com>
2024-07-16 15:59:34 +0900

    [level 2] Title: 괄호 회전하기, Time: 9.46 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+unordered_map<char, char> bracketPair = {{')', '('}, {'}', '{'}, {']', '['}};
+
+bool isValid(string& s, int start)
+{
+    stack<char> stk;
+    unsigned int sz = s.size();
+    
+    for(int i = 0; i < sz; i++)
+    {
+        char ch = s[(start + i) % sz];
+        
+        if(bracketPair.count(ch))
+        {
+            if(stk.empty() || stk.top() != bracketPair[ch]) return false;
+            stk.pop();
+        }
+        else
+        {
+            stk.push(ch);
+        }
+    }
+    
+    return stk.empty();
+}
+
+int solution(string s) 
+{
+    int answer = 0;
+    int n = s.size();
+    
+    for(int i = 0; i < n; i++)
+    {
+        answer += isValid(s,i);
+    }
+    return answer;
+}
